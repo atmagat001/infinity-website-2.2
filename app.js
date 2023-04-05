c007ef29a695802f7f6ea151f88ecee3c991a1f2
atmagat001 <atmagat@gmail.com>
2023-04-06 00:21:42 +0800

    adjusted the burger menu

---
+++ app.js
@@ -0,0 +1,24 @@
+const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
+const mainMenu = document.getElementById("main-menu");
+const menu_items = document.querySelectorAll('nav main-menu ul li a');
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", hide);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        hide();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function hide(){
+    mainMenu.style.top = "-100%";
+    
+    
+}
